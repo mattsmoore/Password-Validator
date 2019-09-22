112933bd56d83dd9ac252a624a09139c9ff772a3
mmoore01 <31735403+mmoore01@users.noreply.github.com>
2019-09-22 19:40:58 -0300

    Added more comments

--- app/src/main/java/com/example/password_validator/Validator.java
+++ app/src/main/java/com/example/password_validator/Validator.java
@@ -2,13 +2,17 @@ package com.example.password_validator;
 
 class Validator {
 
+    // basic validator
     static int basicValidator(String s){
 
         int rules = 0;
 
+        // at least 8 characters long
         if(!s.matches(".{8,}")){
             rules++;
         }
+
+        // is not a variation of the word "Password"
         if(s.toLowerCase().matches("(password)")){
             rules++;
         }
@@ -23,18 +27,27 @@ class Validator {
         int rules = 0;
 
 
+        // is more than 8 characters long
         if(!s.matches(".{8,}")){
             rules++;
         }
+
+        // is not a variation of the word "Password"
         if(s.toLowerCase().matches("(password)")){
             rules++;
         }
+
+        // contains at least one number
         if(!s.matches(".*[0-9].*")){
             rules++;
         }
+
+        // contains at least one special character
         if(!s.matches(".*[!@#$%^&*(),.?\":{}|<>].*")){
             rules++;
         }
+
+        // contains at least one capital letter
         if(!s.matches(".*[A-Z]+.*")){
             rules++;
         }
